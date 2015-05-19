ac5f2b0610b326beafea6a82c44ed23b37a257e0
ELODIE HESLOUIS <Didie@elodie-heslouiss-macbook.local>
2015-05-19 11:48:19 +0700

    Feature
    Move navbar in php

--- navbar.php
+++ navbar.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="description" content="Coup de Pouce">
-        <meta name="author" content="Elodie Heslouis">
-        <!-- <link rel="icon" href="../../favicon.ico"> -->
-        <title>Coup de Pouce</title>
-        <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="js/ie-emulation-modes-warning.js"></script>
-        <!-- Custom styles for this template -->
-        <link href="css/main.css" rel="stylesheet">
-        <link href="css/navbar.css" rel="stylesheet">
-    </head>
-
-    <body>
-        <div class="container">
-
 <!-- ================ NAVBAR ================ -->
             <div class="row">
                 <div class="col-md-12">
@@ -65,25 +43,3 @@
                     </div>
                 </div>
             </div>
-
-    <!-- ================ CAROUSEL ================ -->
-            <div>
-            </div>
-
-
-
-
-
-
-        </div>
-
-<!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-
-    </body>
-</html>
