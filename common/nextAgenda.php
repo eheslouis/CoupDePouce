1cbc681c6a964c9781692864517f5b4853d753aa
ELODIE HESLOUIS <Didie@elodie-heslouiss-macbook.local>
2015-07-28 13:57:09 +0700

    move to php

---
+++ common/nextAgenda.php
@@ -0,0 +1,8 @@
+<!-- ================ NEXT AGENDA ================ -->
+            <div class="row">
+                <div class="col-md-12 agenda">
+                    <p class="pull-right">
+                        Prochain rendez-vous: Vente de livre le 30 Mai 2015
+                    </p>
+                </div>
+            </div>
--- index.php
+++ index.php
@@ -19,74 +19,14 @@
 
     <body>
         <div class="container">
-<!-- ================ NEXT AGENDA ================ -->
-            <div class="row">
-                <div class="col-md-12 agenda">
-                    <p class="pull-right">
-                        Prochain rendez-vous: Vente de livre le 30 Mai 2015
-                    </p>
-                </div>
-            </div>
-
-<!-- ================ NAVBAR ================ -->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="navbar-wrapper">
-                        <nav class="navbar navbar-default navbar-static-top">
-                            <div class="navbar-header">
-                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                                    <span class="glyphicon glyphicon-menu-hamburger" aria-labe="menu"></span>
-                                </button>
-                                <a href="index.html"><img class="navbar-logo" src="images/logo.jpg" alt="logo coup de pouce"></a>
-                            </div>
-                            <div id="navbar" class="navbar-collapse collapse">
-                                <ul class="nav navbar-nav">
-                                    <li class="active"><a href="index.html">Accueil</a></li>
-                                    <li><a href="#assoc">Notre Association</a></li>
-                                    <li class="dropdown">
-                                        <a href="#actu" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Notre Actualité <b class="caret"></b></a>
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li class="sub-item-menu"><a href="#">La vie de l'association</a></li>
-                                            <li class="sub-item-menu"><a href="#">Agenda</a></li>
-                                            <li class="sub-item-menu"><a href="#">Galerie photo</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#parrainage">Le Parrainage</a></li>
-
-                                    <li class="dropdown">
-                                        <a href="#projet" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Nos Projets <b class="caret"></b></a>
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li class="dropdown-header">Soutien à nos familles parrainées</li>
-                                            <li class="sub-item-menu"><a href="#">Bouse d'étude universitaire</a></li>
-                                            <li class="sub-item-menu"><a href="#">Amélioration de l'habitat</a></li>
-                                            <li class="divider"></li>
-                                            <li class="dropdown-header">Autres projets</li>
-                                            <li class="sub-item-menu"><a href="#">Meo-Vac</a></li>
-                                            <li class="sub-item-menu"><a href="#">Phuc Tue</a></li>
-                                            <li class="sub-item-menu"><a href="#">Blue Dragon</a></li>
-                                            <li class="sub-item-menu"><a href="#">School on the Boat</a></li>
-                                            <li class="divider"></li>
-                                            <li><a href="#">Archives</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown">
-                                        <a href="#action" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Votre Action <b class="caret"></b></a>
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li class="sub-item-menu"><a href="#">Comment participer</a></li>
-                                            <li class="sub-item-menu"><a href="#">Dons - Sponsor</a></li>
-                                            <li class="sub-item-menu"><a href="#">Boutique</a></li>
-                                        </ul>
-                                    </li>
-
-                                    <li class="social"><a href="#" class="zocial-facebook"></a></li>
-                                    <li class="social"><a href="#" class="zocial-twitter"></a></li>
-                                    <li class="social"><a href="#" class="zocial-email"></a></li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+            <?php
+                include "common/nextAgenda.php";
+                echo "<div>".$thedifferentpart."</div>";
+            ?>
+            <?php
+                include "common/navbar.php";
+                echo "<div>".$thedifferentpart."</div>";
+            ?>
 
 <!-- ================ CAROUSEL ================ -->
             <div class="row">
