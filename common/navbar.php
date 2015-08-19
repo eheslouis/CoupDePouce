18e5115e5346fefc1ef38476f1aaf642c6e45f60
ELODIE HESLOUIS <Didie@elodie-heslouiss-macbook.local>
2015-08-19 15:40:10 +0700

    feature
    change to php
    create new pages

--- common/navbar.php
+++ common/navbar.php
@@ -7,50 +7,50 @@
                                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                                     <span class="glyphicon glyphicon-menu-hamburger" aria-labe="menu"></span>
                                 </button>
-                                <a href="index.html"><img class="navbar-logo" src="images/logo.jpg" alt="logo coup de pouce"></a>
+                                <a href="index.php"><img class="navbar-logo" src="images/logo.jpg" alt="logo coup de pouce"></a>
                             </div>
                             <div id="navbar" class="navbar-collapse collapse">
                                 <ul class="nav navbar-nav">
-                                    <li class="active"><a href="index.html">Accueil</a></li>
-                                    <li><a href="#assoc">Notre Association</a></li>
+                                    <li class="active"><a href="index.php">Accueil</a></li>
+                                    <li><a href="assoc.php">Notre Association</a></li>
                                     <li class="dropdown">
-                                        <a href="#actu" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Notre Actualité <b class="caret"></b></a>
+                                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Notre Actualité <b class="caret"></b></a>
                                         <ul class="dropdown-menu" role="menu">
-                                            <li class="sub-item-menu"><a href="#">La vie de l'association</a></li>
-                                            <li class="sub-item-menu"><a href="#">Agenda</a></li>
-                                            <li class="sub-item-menu"><a href="#">Galerie photo</a></li>
+                                            <li class="sub-item-menu"><a href="vieAssoc.php">La vie de l'association</a></li>
+                                            <li class="sub-item-menu"><a href="agenda.php">Agenda</a></li>
+                                            <li class="sub-item-menu"><a href="galeriePhoto.php">Galerie photo</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="#parrainage">Le Parrainage</a></li>
+                                    <li><a href="parrainage.php">Le Parrainage</a></li>
 
                                     <li class="dropdown">
-                                        <a href="#projet" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Nos Projets <b class="caret"></b></a>
+                                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Nos Projets <b class="caret"></b></a>
                                         <ul class="dropdown-menu" role="menu">
                                             <li class="dropdown-header">Soutien à nos familles parrainées</li>
-                                            <li class="sub-item-menu"><a href="#">Bouse d'étude universitaire</a></li>
-                                            <li class="sub-item-menu"><a href="#">Amélioration de l'habitat</a></li>
+                                            <li class="sub-item-menu"><a href="bourse.php">Bouse d'étude universitaire</a></li>
+                                            <li class="sub-item-menu"><a href="habitat.php">Amélioration de l'habitat</a></li>
                                             <li class="divider"></li>
                                             <li class="dropdown-header">Autres projets</li>
-                                            <li class="sub-item-menu"><a href="#">Meo-Vac</a></li>
-                                            <li class="sub-item-menu"><a href="#">Phuc Tue</a></li>
-                                            <li class="sub-item-menu"><a href="#">Blue Dragon</a></li>
-                                            <li class="sub-item-menu"><a href="#">School on the Boat</a></li>
+                                            <li class="sub-item-menu"><a href="meovac.php">Meo-Vac</a></li>
+                                            <li class="sub-item-menu"><a href="phuctue.php">Phuc Tue</a></li>
+                                            <li class="sub-item-menu"><a href="bluedragon.php">Blue Dragon</a></li>
+                                            <li class="sub-item-menu"><a href="sob.php">School on the Boat</a></li>
                                             <li class="divider"></li>
-                                            <li><a href="#">Archives</a></li>
+                                            <li><a href="archives.php">Archives</a></li>
                                         </ul>
                                     </li>
                                     <li class="dropdown">
-                                        <a href="#action" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Votre Action <b class="caret"></b></a>
+                                        <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Votre Action <b class="caret"></b></a>
                                         <ul class="dropdown-menu" role="menu">
-                                            <li class="sub-item-menu"><a href="#">Comment participer</a></li>
-                                            <li class="sub-item-menu"><a href="#">Dons - Sponsor</a></li>
-                                            <li class="sub-item-menu"><a href="#">Boutique</a></li>
+                                            <li class="sub-item-menu"><a href="participer.php">Comment participer</a></li>
+                                            <li class="sub-item-menu"><a href="dons.php">Dons - Sponsor</a></li>
+                                            <li class="sub-item-menu"><a href="boutique.php">Boutique</a></li>
                                         </ul>
                                     </li>
 
                                     <li class="social"><a href="#" class="zocial-facebook"></a></li>
                                     <li class="social"><a href="#" class="zocial-twitter"></a></li>
-                                    <li class="social"><a href="#" class="zocial-email"></a></li>
+                                    <li class="social"><a href="contact.php" class="zocial-email"></a></li>
                                 </ul>
                             </div>
                         </nav>
---
+++ habitat.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="description" content="Coup de Pouce">
+        <meta name="author" content="Elodie Heslouis">
+        <!-- <link rel="icon" href="../../favicon.ico"> -->
+        <title>Coup de Pouce</title>
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/ie-emulation-modes-warning.js"></script>
+        <!-- Custom styles for this template -->
+        <link href="css/main.css" rel="stylesheet">
+        <link href="css/navbar.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <?php
+                include "common/nextAgenda.php";
+                echo "<div>".$thedifferentpart."</div>";
+            ?>
+            <?php
+                include "common/navbar.php";
+                echo "<div>".$thedifferentpart."</div>";
+            ?>
+        </div>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    </body>
+</html>
